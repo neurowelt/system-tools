d75823406a5dda00c4920437bcb03acccb6058ab
neurowelt <kamilklecha96@gmail.com>
2024-08-02 15:02:47 +0200

    feat: basic architecture information script

---
+++ architecture.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+get_os () {
+    echo "$(uname -o)"
+}
+
+get_machine () {
+    echo "$(uname -m)"
+}
+
+main() {
+    os=$(get_os)
+    machine=$(get_machine)
+    echo "Your machine type is: $machine"
+    echo "Your operating system is: $os"
+}
+
+if [[ ${BASH_SOURCE[0]} == ${0} ]]; then
+    main "$@"
+fi
